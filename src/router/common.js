4fe9bda7df8cba89cc920285d15e60aaaac4a8c6
yangang <yang@ktwelve.com.cn>
2021-06-11 17:39:57 +0800

    fix: 修改路由参数

--- src/router/common.js
+++ src/router/common.js
@@ -1,14 +1,11 @@
 export default [
 {
+  path: '/',
+  name: 'login',
+  component: () => import('@/views/login/index.vue')
+},
+{
   path: '/home',
   name: 'home',
   component: () => import('@/views/home/index.vue')
-},
-{
-  path: '/',
-  name: 'login',
-  component: () => import('@/views/login/index.vue'),
-  meta: {
-    inPageLayout: true
-  }
 }]
