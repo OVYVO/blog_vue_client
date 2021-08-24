b2346c7b94ee9d7ff3d2906ece30fd3469f68f8a
yangang <yang@ktwelve.com.cn>
2021-08-24 09:40:00 +0800

    fix: 修改依赖

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ import 'element-plus/lib/theme-chalk/index.css'
 import '@/styles/index.less'
 import 'es6-promise/auto'
 
-import VueMarkdownEditor from '@kangc/v-md-editor';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
-import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
-import Prism from 'prismjs';
+// import VueMarkdownEditor from '@kangc/v-md-editor';
+// import '@kangc/v-md-editor/lib/style/base-editor.css';
+// import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+// import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+// import Prism from 'prismjs';
 
-VueMarkdownEditor.use(vuepressTheme, {
-  Prism
-});
+// VueMarkdownEditor.use(vuepressTheme, {
+//   Prism
+// });
 
 const app = createApp(App)
 app.config.productionTip = false;
